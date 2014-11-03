296210e083d7b302550ed811a5e74a7a47bdb3a4
Blaise Fonseca <blaisefonseca@hotmail.com>
2014-11-03 11:51:39 -0500

    add role and country filter to user list

--- app/views/admin/users.html.js
+++ app/views/admin/users.html.js
@@ -1,4 +1,4 @@
-function UsersController ($rootScope, $scope, authHttp, $route, $location) {
+function UsersController ($rootScope, $scope, authHttp, $route, $location, $filter) {
 
     if(!$rootScope.user.isAuthenticated) {  //navigation.securize();
         $location.path('/signin');
@@ -10,15 +10,15 @@ function UsersController ($rootScope, $scope, authHttp, $route, $location) {
         return;
     }
 
-	authHttp.get('/api/v2013/roles', { cache: true }).then(function (response) { 
+	authHttp.get('/api/v2013/roles', { cache: true }).then(function (response) {
 		var map = {};
 		response.data.forEach(function (role) {
 			map[role.roleId] = role;
 		});
-		$scope.roles = map; 
+		$scope.roles = map;
 	});
 
-	authHttp.get('/api/v2013/countries', { cache: true }).then(function (response) { 
+	authHttp.get('/api/v2013/countries', { cache: true }).then(function (response) {
 		var map = {};
 		response.data.forEach(function (country) {
 			country.name = country.name.en;
@@ -27,14 +27,29 @@ function UsersController ($rootScope, $scope, authHttp, $route, $location) {
 
 		map["EUR"] = map["EU"];//patch BCH
 
-		$scope.countries = map; 
+		$scope.countries = map;
+    $scope.countriesList=[];
+    $filter('orderBy')(response.data, 'name.en').forEach(function (country) {
+
+          $scope.countriesList.push({ code : country.code.toLowerCase(), name: country.name });
+    });
 	});
 
+  authHttp.get("/api/v2013/roles", { cache: true }).then(function(response) {
+      $scope.roleList = [];
+      $filter('orderBy')(response.data, 'name').forEach(function (o) {
+                            $scope.roleList.push({ roleId : o.roleId, name: o.name });
+                        });
+
+  });
+
 	function populate () {
-		authHttp.get('/api/v2013/users', { params: { q: $scope.freetext, sk: $scope.currentPage*25, l: 25 } }).then(function (response) {
-			$scope.users = response.data;
-			$scope.users.forEach(function (user) { if(user.government) user.government = user.government.toUpperCase(); });
-		});
+		authHttp.get('/api/v2013/users', { params: { q: $scope.freetext, sk: $scope.currentPage*25, l: 25 ,
+                                                  government : $scope.government, role: $scope.roleFilter} })
+            .then(function (response) {
+          			$scope.users = response.data;
+          			$scope.users.forEach(function (user) { if(user.government) user.government = user.government.toUpperCase(); });
+        		});
 	};
 
 	function setPage (page) {
@@ -46,7 +61,7 @@ function UsersController ($rootScope, $scope, authHttp, $route, $location) {
 	//
 	//============================================================
 	$scope.actionDelete = function actionDelete (userID) {
-		
+
 		if (confirm('Are you sure you want to delete this user account?')) {
 
 			authHttp.delete('/api/v2013/users/' + userID).success(function (data, status, headers, config) {
@@ -65,5 +80,7 @@ function UsersController ($rootScope, $scope, authHttp, $route, $location) {
     $scope.actionSetPage = setPage;
 
 	$scope.$watch('freetext', populate);
+  $scope.$watch('government', populate);
+  $scope.$watch('roleFilter', populate);
 	$scope.$watch('currentPage', populate);
 }
