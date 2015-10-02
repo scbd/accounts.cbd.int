3c083ec20fa2dd2b8723013d1a0c9bcc56e238b5
unknown <randy.houlahan@Houlahan.biodiv.org>
2015-10-02 11:13:26 -0400

    Update to tabs of user pages limiting to set of 10 pages

--- app/views/admin/users.js
+++ app/views/admin/users.js
@@ -3,41 +3,43 @@ define(['app', 'lodash', 'authentication'], function(app, _) { 'use strict';
     return ['$scope', '$http', '$route', '$location', '$filter', '$q', function($scope, $http, $route, $location, $filter, $q) {
 
     $scope.pageSize = 25;
+    $scope.tabSetSize = 10;
+    $scope.set = 0;
     updatePager(0);
 
-    $http.get('/api/v2013/roles', { cache: true }).then(function (response) {
-		var map = {};
-		response.data.forEach(function (role) {
-			map[role.roleId] = role;
-		});
-		$scope.roles = map;
-	});
+  $http.get('/api/v2013/roles', { cache: true }).then(function (response) {
+    		var map = {};
+    		response.data.forEach(function (role) {
+    			map[role.roleId] = role;
+    		});
+    		$scope.roles = map;
+	});//  $http.get('/api/v2013/roles', { cache: true }).
 
-    $http.get('/api/v2013/countries', { cache: true }).then(function (response) {
-		var map = {};
-		response.data.forEach(function (country) {
-			country.name = country.name.en;
-			map[country.code] = country;
-		});
+  $http.get('/api/v2013/countries', { cache: true }).then(function (response) {
+  		var map = {};
+  		response.data.forEach(function (country) {
+  			country.name = country.name.en;
+  			map[country.code] = country;
+  		});
 
-		map.EUR = map.EUR || map.EU;
+  		map.EUR = map.EUR || map.EU;
 
-		$scope.countries = map;
-        $scope.countriesList=[];
+  		$scope.countries = map;
+      $scope.countriesList=[];
 
-        $filter('orderBy')(response.data, 'name').forEach(function (country) {
-            $scope.countriesList.push({ code : country.code.toLowerCase(), name: country.name });
-        });
-	});
+      $filter('orderBy')(response.data, 'name').forEach(function (country) {
+          $scope.countriesList.push({ code : country.code.toLowerCase(), name: country.name });
+      });
+	});//  $http.get('/api/v2013/countries',
 
-    $http.get("/api/v2013/roles", { cache: true }).then(function(response) {
-        $scope.roleList = [];
-        $filter('orderBy')(response.data, 'name').forEach(function (o) {
-            $scope.roleList.push({ roleId : o.roleId, name: o.name });
-        });
-    });
+  $http.get("/api/v2013/roles", { cache: true }).then(function(response) {
+      $scope.roleList = [];
+      $filter('orderBy')(response.data, 'name').forEach(function (o) {
+          $scope.roleList.push({ roleId : o.roleId, name: o.name });
+      });
+  });//$http.get("/api/v2013/roles", {
 
-    var cancelPopulate;
+  var cancelPopulate;
 
 	function populate () {
 
@@ -72,28 +74,40 @@ define(['app', 'lodash', 'authentication'], function(app, _) { 'use strict';
         });
 	}
 
-    //============================================================
+  //============================================================
 	//
 	//
 	//============================================================
-    function setPage (page) {
-		$scope.currentPage = Math.max(0, Math.min($scope.pageCount-1, page|0));
+  function setPage (page) {
+
+      $scope.currentPage = Math.max(0, Math.min($scope.pageCount-1, page|0));
 	}
 
-    //============================================================
+  //============================================================
+  //
+  //
+  //============================================================
+    function calcSet (page) {
+
+        var sets = _.range(0,$scope.pageCount,$scope.tabSetSize);
+        return  _.find(sets, function(setStart){
+                      return (page >= setStart && page < setStart+($scope.tabSetSize)); });
+  }//calcSet (page) {
+
+  //============================================================
 	//
 	//
 	//============================================================
     function updatePager(currentPage) {
 
-        var userCount = $scope.userCount || ($scope.users || []).length;
-
+        var userCount = $scope.userCount || ($scope.users || []).length || $scope.tabSetSize;
         $scope.pageCount = Math.ceil(userCount / 25);
-        $scope.pages     = _.range($scope.pageCount);
-
+        $scope.set=calcSet(currentPage || $scope.currentPage || 0);
+        $scope.pages     = _.range($scope.pageCount).splice($scope.set,($scope.tabSetSize));
         if(currentPage!==undefined)
-            setPage(currentPage);
-	}
+            setPage (currentPage);
+
+	}//updatePager(currentPage)
 
 	//============================================================
 	//
@@ -101,25 +115,25 @@ define(['app', 'lodash', 'authentication'], function(app, _) { 'use strict';
 	//============================================================
 	$scope.actionDelete = function actionDelete (userID) {
 
-		if (confirm('Are you sure you want to delete this user account?')) {
+    		if (confirm('Are you sure you want to delete this user account?')) {
 
-            $http.delete('/api/v2013/users/' + userID).success(function () {
-				populate();
-				alert('The user account has been deleted.');
-	        }).error(function (data) {
-    	    	alert(data);
-        	});
-	    }
-	};
+                $http.delete('/api/v2013/users/' + userID).success(function () {
+    				populate();
+    				alert('The user account has been deleted.');
+    	        }).error(function (data) {
+        	    	alert(data);
+            	});
+    	  }
+	};$scope.actionDelete = function actionDelete (userID)
 
-    $scope.actionSetPage = setPage;
-
-    $scope.$watch('government',  populate);
-    $scope.$watch('roleFilter',  populate);
+  $scope.actionSetPage = setPage;
+  $scope.$watch('government',  populate);
+  $scope.$watch('roleFilter',  populate);
 	$scope.$watch('currentPage', populate);
+
 	$scope.$watch('freetext',  _.debounce(function(){
         setPage(0);
         populate();
     }, 250));
-}];
+  }];
 });
