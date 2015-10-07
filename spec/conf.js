8be467551096d18f38584d2303cc7ae805b0f538
Philippe Leblond <leblond.philippe@gmail.com>
2015-10-07 16:53:20 -0400

    Run tests on localhost

--- spec/conf.js
+++ spec/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     framework: 'jasmine2',
-    seleniumAddress: 'http://selenium:4444/wd/hub',
-    baseUrl: 'http://node:8000',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    baseUrl: 'http://localhost:8000',
     specs: ['*[sS]pec.js']
 }
