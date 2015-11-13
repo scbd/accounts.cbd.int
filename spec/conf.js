310c8fca8ba2b9a5014f4431dbda3ef7d06e3644
Philippe Leblond <philippe.leblond@cbd.int>
2015-11-13 12:00:37 -0500

    Run tests on Open Sauce (Chrome, Firefox, IE and Safari)

--- spec/conf.js
+++ spec/conf.js
@@ -2,5 +2,22 @@ exports.config = {
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:8000',
-    specs: ['*[sS]pec.js']
+    specs: ['*[sS]pec.js'],
+    multiCapabilities: [{
+        browserName: 'chrome',
+        platform: 'Windows 7',
+        version: '45.0'
+    }, {
+        browserName: 'firefox',
+        platform: 'Windows 7',
+        version: '41.0'
+    }, {
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '9.0'
+    }, {
+        browserName: 'safari',
+        platform: 'OS X 10.10',
+        version: '8.0';
+    }]
 }
