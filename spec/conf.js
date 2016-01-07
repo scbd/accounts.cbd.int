e4089383f6fdf3d61a04215bc0684913348a2bc8
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-06 22:10:21 -0500

    Update config for new spec

--- spec/conf.js
+++ spec/conf.js
@@ -1,25 +1,33 @@
 exports.config = {
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
-    framework: 'jasmine2',
-    sauceSeleniumAddress: 'localhost:4444/wd/hub',
-    baseUrl: 'http://localhost:8000',
-    specs: ['*[sS]pec.js'],
-    multiCapabilities: [{
-        browserName: 'chrome',
-        platform: 'Windows 7',
-        version: '45.0'
-    }, {
-        browserName: 'firefox',
-        platform: 'Windows 7',
-        version: '41.0'
-    }, {
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '10.0'
-    }, {
-        browserName: 'safari',
-        platform: 'OS X 10.10',
-        version: '8.0'
-    }]
-}
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  framework: 'jasmine2',
+  sauceSeleniumAddress: 'localhost:4444/wd/hub',
+  baseUrl: 'http://localhost:8000',
+  // Selector for the element housing the angular app - this defaults to
+  // body, but is necessary if ng-app is on a descendant of <body>
+  // solved intermintent fails
+  rootElement: 'body',
+  specs: ['*[sS]pec.js'],
+  //  maxSessions: 1,
+  multiCapabilities: [{
+    browserName: 'chrome',
+    platform: 'Windows 7',
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '9.0'
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.10',
+  }],
+  onPrepare: function() {
+    browser.driver.manage().window().maximize();
+    return browser.getCapabilities().then(function(cap) {
+      browser.browserName = cap.caps_.browserName;
+    });
+  }
+};
