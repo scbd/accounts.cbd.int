e352bbefeb2b99916d8260cb25d2e5a036cdd2dc
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-01-08 16:33:08 -0500

    Fix IE console

--- app/boot.js
+++ app/boot.js
@@ -25,4 +25,7 @@ curl(['angular', 'app', 'routes', 'template', 'authentication', 'providers/exten
 
 })();
 
+// Fix Function.prototype.bind
 Function.prototype.bind=Function.prototype.bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}var a=Array.prototype.slice,f=a.call(arguments,1),e=this,c=function(){},d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
+// Fix IE Console
+(function(a){a.console||(a.console={});for(var c="log info warn error debug trace dir group groupCollapsed groupEnd time timeEnd profile profileEnd dirxml assert count markTimeline timeStamp clear".split(" "),d=function(){},b=0;b<c.length;b++)a.console[c[b]]||(a.console[c[b]]=d)})(window); //jshint ignore:line
