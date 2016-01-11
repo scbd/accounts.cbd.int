42f2b5eeb79fe54254656a3f8a982349eb9699b4
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-01-11 12:10:04 -0500

    Rollback from curljs to requirejs

--- app/boot.js
+++ app/boot.js
@@ -1,31 +1,31 @@
-(function(){ 'use strict';
+(function(window){ 'use strict';
 
-curl.config({
-    waitSeconds: 120,
+require.config({
+    waitSeconds: 60,
     baseUrl : '/app',
+    pluginPath: 'libs/curl/src/curl/plugin',
     paths: {
         'async'           : 'libs/requirejs-plugins/src/async',
-        'text'            : 'libs/requirejs-text/text',
         'lodash'          : 'libs/lodash/lodash.min',
+        'text'            : 'libs/text/text',
         'urijs'           : 'libs/urijs/src'
-    },
-    plugins: {
-        'text'            : 'libs/curl/src/curl/plugin/text'
     }
 });
 
-define('angular', window.angular);
+if (!require.defined('angular')) {
+    define('angular', [], function() { 
+        return window.angular;
+    });
+}
 
-curl(['angular', 'app', 'routes', 'template', 'authentication', 'providers/extended-route']).then(function (ng, app) {
+require(['angular', 'app', 'routes', 'template', 'authentication', 'providers/extended-route'], function (ng, app) {
 
     ng.element(document).ready(function () {
          ng.bootstrap(document, [app.name]);
     });
 });
 
-})();
+})(window);
 
-// Fix Function.prototype.bind
-Function.prototype.bind=Function.prototype.bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}var a=Array.prototype.slice,f=a.call(arguments,1),e=this,c=function(){},d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
 // Fix IE Console
 (function(a){a.console||(a.console={});for(var c="log info warn error debug trace dir group groupCollapsed groupEnd time timeEnd profile profileEnd dirxml assert count markTimeline timeStamp clear".split(" "),d=function(){},b=0;b<c.length;b++)a.console[c[b]]||(a.console[c[b]]=d)})(window); //jshint ignore:line
