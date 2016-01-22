2042a2134be1af713fd4df3852ba9b319b3951d9
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-01-22 11:44:06 -0500

    display error message for signup
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- app/views/signup.html.js
+++ app/views/signup.html.js
@@ -1,38 +1,5 @@
-require('app').controller('SignupController', ['$scope', '$http', '$location', '$filter','referrer', function ($scope, $http, $location, $filter,referrer) {
+require('app').controller('SignupController', ['$scope', '$http', '$location', '$filter', function ($scope, $http, $location, $filter) {
     $scope.passwordType = 'password';
-    $scope.options = { countries: [] };
-
-    $http.get("/api/v2013/thesaurus/domains/countries/terms", { cache: true }).then(function(o) {
-        $scope.options.countries = $filter('orderBy')(o.data, 'name');
-    });
-console.log(referrer.get());
-    //==================================
-    //
-    //==================================
-    $scope.init = function() {
-
-        if($route.current.params.id=='new') {
-            $scope.initialRoles = [];
-        } else {
-            $http.get('/api/v2013/users/'+$route.current.params.id).success(function (data, status, headers, config) {
-                $scope.document = data;
-                $scope.loadPhones();
-                $scope.loadFaxes();
-                $scope.loadEmails();
-            }).error(function (data, status, headers, config) {
-                alert('ERROR\r\n----------------\r\n'+data);
-            });
-
-            $http.get('/api/v2013/users/'+$route.current.params.id+'/roles').success(function (data, status, headers, config) {
-                $scope.roles = data;
-                $scope.initialRoles = data.slice(0); // clone array
-            }).error(function (data, status, headers, config) {
-                alert('ERROR\r\n----------------\r\n'+data);
-            });
-        }
-    };
-
-
     //============================================================
     //
     //
