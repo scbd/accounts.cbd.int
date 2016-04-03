2ef6d1147a909fb36f0864102be7a116095b1472
Randy Houlahan <randy.houlahan@cbd.int>
2016-04-03 07:45:48 -0400

    When no back uri case and $location error

--- app/views/profile-done.js
+++ app/views/profile-done.js
@@ -1,21 +1,22 @@
 define(['app', 'angular'], function(app, angular) {
 
-  return ['$scope', '$location', '$window', '$timeout', function($scope, authHttp, $location, $window, $timeout) {
+  return ['$scope', '$location', '$window', '$timeout', function($scope,  $location, $window, $timeout) {
 
     $scope.returnUrl = $location.search().returnurl || $location.search().returnUrl || $location.search().redirect_uri || '/';
     $scope.redirectTime = 5;
 
-    var cancel = setInterval(function() {
+    if($scope.returnUrl !== '/')
+      var cancel = setInterval(function() {
 
-      if ($scope.redirectTime === 0) {
-        console.log($scope.redirectTime);
-        clearInterval(cancel);
-        $window.location.href = $scope.returnUrl;
-      }
-      $timeout(function() {
-        if ($scope.redirectTime) $scope.redirectTime--;
-      });
-    }, 1000);
+        if ($scope.redirectTime === 0) {
+          console.log($scope.redirectTime);
+          clearInterval(cancel);
+          $window.location.href = $scope.returnUrl;
+        }
+        $timeout(function() {
+          if ($scope.redirectTime) $scope.redirectTime--;
+        });
+      }, 1000);
 
   }];
 });
