9a0435ee7c55f3cfd3e08c1fd54e4ef3b79b7701
Philippe Leblond <philippe.leblond@cbd.int>
2016-04-15 10:44:30 -0400

    Change HOST to api.cbddev.xyz

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/app/libs', express.static(__dirname + '/app/libs'));
 app.use('/app',      express.static(__dirname + '/app'));
 app.get('/app/*', (req, res) => res.status(404).send("404 - Not Found"));
 
-app.all('/api/*', (req, res) => proxy.web(req, res, { target: 'https://api.cbd.int:443', secure: false }));
+app.all('/api/*', (req, res) => proxy.web(req, res, { target: 'https://api.cbddev.xyz', changeOrigin: true }));
 ///non angularjs file for activating email
 app.get('/activate', (req, res) => res.sendFile(__dirname + '/app/views/activate.html'));
 
