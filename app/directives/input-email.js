f06e2bad5c0d02639716250ff873b52695660854
Stephane Bilodeau <bilodeaustephane@gmail.com>
2017-03-22 16:56:26 -0400

    Form input[type=email] angular form validator

---
+++ app/directives/input-email.js
@@ -0,0 +1,27 @@
+define(['app'], function(app){
+
+    // TODO: ENSURE CONSISTENCY WITH BACK-END
+    var EMAIL_REGEXP = /^[!#$%&'*+\-\/=?^_`{|}~a-z0-9]+(\.*[!#$%&'*+\-\/=?^_`{|}~a-z0-9]+)*@[a-z0-9]+([-\.][a-z0-9]+)*\.[a-z]{2,}$/i;
+
+    app.directive('type', [function() {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+
+                if(!ctrl)  return;
+                if( attrs.type!='email') return;
+
+                ctrl.$validators.email = function(modelValue, viewValue) {
+
+                    if (ctrl.$isEmpty(modelValue)) {
+                        return true;
+                    }
+
+                    return !!EMAIL_REGEXP.test(viewValue);
+                };
+            }
+        };
+    }]);
+
+});
