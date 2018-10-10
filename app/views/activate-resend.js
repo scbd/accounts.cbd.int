a660af8d1c69c3e08d781d3bccc950023219020a
Randy Houlahan <randy.houlahan@cbd.int>
2018-10-10 14:35:05 -0400

    $http changes on library upgrade

--- app/views/activate-resend.js
+++ app/views/activate-resend.js
@@ -8,11 +8,11 @@ return ['$scope', '$http', '$location', function ($scope, $http, $location) {
     //============================================================
     $scope.actionSubmit = function() {
 
-        $http.post('/api/v2013/users/'+encodeURIComponent($scope.email)+'/activations', angular.toJson({})).success(function onsuccess(data, status, headers, config) {
+        $http.post('/api/v2013/users/'+encodeURIComponent($scope.email)+'/activations', angular.toJson({})).then(function onsuccess(data, status, headers, config) {
 
             $location.path('/signup/done');
 
-        }).error(function (data, status, headers, config) {
+        }).catch(function (data, status, headers, config) {
 
             $scope.error = status;
         });
