a45d1ab5f40edb129fc5c0ec8649e750c4d891ce
Blaise Fonseca <blaisefonseca@hotmail.com>
2019-06-18 16:09:28 -0400

    authorize on main domain instead of apps

--- app/authorize-test.js
+++ app/authorize-test.js
@@ -1,44 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<body>
-<script>
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-function setCookie(cname, cvalue, exdays)
-{
-    if(cvalue===null || cvalue === undefined)
-        exdays = -365;
-    var parts = [cname + "=" + (cvalue||"")];
-    if(exdays) {
-        var d = new Date();
-        if(typeof exdays == 'string' && /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{1,8}Z/.test(exdays))
-            d = new Date(exdays)
-        else      
-            d.setDate(d.getDate()+exdays);
-        parts.push("expires="+d.toGMTString())
-    }    
-    parts.push("path=/");
-    document.cookie =  parts.join("; ");
-}
+﻿var document = { location : { hostname: '' }  };
+
+var DOMAINS = ["cbd.int", "staging.cbd.int", "cbddev.xyz"];
+var HOSTS   = ["www", "absch", "chm", "bch", "beta.bch"];
+
 var trustedHosts = [
     [/^staging\.cbd\.int$/i, /.*\.staging\.cbd\.int$/i],
     [/^cbddev\.xyz$/i, /.*\.cbddev\.xyz$/i],
     [/^cbd\.int$/i, /.*\.cbd\.int$/i]
 ];
+
+for(domain of DOMAINS) {
+
+    document.location.hostname = `accounts.${domain}`;
+
+    for(otherDomain of DOMAINS) {
+
+        receiveMessage({origin: `http://localhost`});
+        receiveMessage({origin: `http://localhost:1234`});
+    
+        for(host        of HOSTS) {
+    //   receiveMessage({origin:  `http://${host}.${otherDomain}`});
+            receiveMessage({origin: `https://${host}.${otherDomain}`});
+        }
+    }
+}
+
 function receiveMessage(event) {
+
     var secureDomain = document.location.hostname.replace(/[^\.]+\./, '');
     var origin = event.origin;
+
     if(origin=='http://localhost' || /^http:\/\/localhost:/.test(origin)) origin = 'https://localhost.'+secureDomain;
+
     var originDomain = origin.match(/(?:^(https)\:\/\/)([^\.]+)\.([^\/]+)/)[3];
-    var originHost   = origin.match(/(?:^(https)\:\/\/)([^\.]+)\.([^\/]+)/)[2];
-    
+    var isTrustedDomain = false;
+
     all:for (var j = 0; j < trustedHosts.length; j++) {
         var domainMatched = false;
         var originMatched = false;
@@ -58,8 +54,15 @@ function receiveMessage(event) {
         if(domainMatched || originMatched)
             break;
     }
+    if(!isTrustedDomain){
+        console.log(`isTrusted: ${isTrustedDomain?'Yes':'No '} - Accessing ${document.location.hostname} from ${event.origin}`);
+    }
+    return console.log(`isTrusted: ${isTrustedDomain?'Yes':'No '} - Accessing ${document.location.hostname} from ${event.origin}`);
+
     if(isTrustedDomain){
+
         var message = JSON.parse(event.data);
+
         if(message.type=='getAuthenticationToken') {
             
             var response = { 
@@ -75,17 +78,17 @@ function receiveMessage(event) {
             }
             window.parent.postMessage(JSON.stringify(response), event.origin);
         }
+
         if(message.type=='setAuthenticationToken') {
+
             if(message.authenticationToken) setCookie('authenticationToken', message.authenticationToken, message.expiration);
             else                            setCookie('authenticationToken', null); //Delete the cookie
+
             if(message.authenticationEmail)        setCookie('email', message.authenticationEmail, 365);
             if(message.authenticationEmail===null) setCookie('email', null); //Delete the cookie
+
             if(message.expiration)        setCookie('expiration', message.expiration, 365);
             else                          setCookie('expiration', null); //Delete the cookie
         }
     }
 }
-window.addEventListener('message', receiveMessage, false);
-</script>
-</body>
-</html>
