bc23e23bd9fd36a1b50f9349f275c3832f246afc
Blaise Fonseca <blaise.fonseca@cbd.int>
2021-05-13 14:24:14 -0400

    add recaptcha to resent activation link

--- app/views/activate-resend.js
+++ app/views/activate-resend.js
@@ -1,4 +1,4 @@
-define(['app', 'directives/input-email'], function(){
+define(['app', 'directives/input-email', './recaptcha.directive.js'], function(){
 
 return ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
@@ -8,14 +8,15 @@ return ['$scope', '$http', '$location', function ($scope, $http, $location) {
     //============================================================
     $scope.actionSubmit = function() {
 
-        $http.post('/api/v2013/users/'+encodeURIComponent($scope.email)+'/activations', angular.toJson({})).then(function onsuccess(data, status, headers, config) {
+        $http.post('/api/v2013/users/'+encodeURIComponent($scope.email)+'/activations', angular.toJson({}), { headers : { 'x-captcha-v2-token' : $scope.grecaptchaToken } })
+        .then(function onsuccess(data, status, headers, config) {
 
             $location.path('/signup/done');
 
         }).catch(function (data, status, headers, config) {
-
             $scope.error = status;
-        });
+            $scope.resetCaptcha();
+        })
     };
 }];
 });
