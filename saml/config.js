9206e7d7834fb7c7842f9ec6b9f31ed5dcb5c7e3
Randy Houlahan <randy.houlahan@cbd.int>
2023-05-02 13:53:23 -0400

    dev instead of local

--- saml/config.js
+++ saml/config.js
@@ -4,8 +4,8 @@ import path from 'path'
 // if(!process.env.APP_DOMAIN)     throw new Error("missing env: APP_DOMAIN")
 
 export const port         = parseInt(process.env.PORT || 8000);
-export const authIssuer   = process.env.AUTH_ISSUER   || 'accounts-saml.localhost';
-export const appDomain    = process.env.APP_DOMAIN    || 'accounts-saml.localhost';
+export const authIssuer   = process.env.AUTH_ISSUER   || 'accounts-saml.cbddev.xyz';
+export const appDomain    = process.env.APP_DOMAIN    || 'accounts-saml.cbddev.xyz';
 export const certPath     = process.env.CERT_PATH     || path.join(path.resolve(), 'certs');
 export const requestTtl   = process.env.REQUEST_TTL   && parseInt(process.env.REQUEST_TTL) || 20 * 1000;
 export const apiUrl       = process.env.API_URL       || 'https://api.cbddev.xyz'
