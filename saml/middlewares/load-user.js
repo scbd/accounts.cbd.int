aa3dd6352a2bec1154c2a885df4c84e31fda47b9
Randy Houlahan <randy.houlahan@cbd.int>
2023-07-14 12:30:47 -0400

    debig headers

--- saml/middlewares/load-user.js
+++ saml/middlewares/load-user.js
@@ -6,6 +6,7 @@ import   winston   from '#~/saml/services/logger.js' ;
 export default  function () {
     return async (req, res, next) => {
         winston.debug('loadUser req.cookies',req.cookies);
+        winston.debug('loadUser req.cookies',req.headers);
         try {
             const { authenticationToken } = req.cookies;
 
