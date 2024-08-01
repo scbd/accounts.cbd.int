919b0d6b0ea94d939f7440ae6c4e17153a4c6e55
Blaise Fonseca <blaise.fonseca@un.org>
2024-08-01 15:15:38 -0400

    Fix dual list control issue

--- app/directives/bootstrap/dual-list.js
+++ app/directives/bootstrap/dual-list.js
@@ -12,10 +12,20 @@ app.directive('duallistbox', [function () {
                 nonselectedlistlabel: 'Non-selected',
                 selectedlistlabel: 'Selected',
                 preserveselectiononmove: 'moved',
-                moveonselect: false
+                moveonselect: false,
+                filterTextClear: 'clear filter'
             });
 
             $scope.$applyAsync(()=>box.trigger('bootstrapduallistbox.refresh'));
+
+            //this is stupid but works :)
+            window.$('.bootstrap-duallistbox-container .box2 .filter.form-control').val('-')
+            window.$('.bootstrap-duallistbox-container .box1 .filter.form-control').val('-')
+            setTimeout(()=>{
+                window.$('.bootstrap-duallistbox-container .btn.clear1').click();
+                window.$('.bootstrap-duallistbox-container .btn.clear2').click();
+                console.log('clear')
+            }, 500)
         }
     };
 }]);
